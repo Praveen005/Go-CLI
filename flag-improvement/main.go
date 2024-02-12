33e965445fbf46f26363c9f5aa26d40470ed8d59
Praveen005 <praveen08h@gmail.com>
2024-02-12 13:44:16 +0530

    adding some files

---
+++ flag-improvement/main.go
@@ -0,0 +1,9 @@
+/*
+	Improving the User Interface:
+
+	- Remove the duplicate error messages
+	- Customize the help usage message
+	- Allow the user to enter their name via a positional argument
+
+
+*/
